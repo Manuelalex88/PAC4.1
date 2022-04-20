b29d96432e50b7759f389627f1918656a2e947e2
Usuario <Usuario@DESKTOP-OA178EB>
2022-04-20 12:46:01 +0200

    Primer Commit

--- JJoseManuelRuizBolaños1/src/JJoseManuelRuizBolaños1.java
+++ JJoseManuelRuizBolaños1/src/JJoseManuelRuizBolaños1.java
@@ -1,11 +1,25 @@
 
 public class JJoseManuelRuizBolaños1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hola mundo");
+	private String nombre;
 
+	public String getNombre() {
+		return nombre;
 	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	private String apellido;
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
 	
 }
