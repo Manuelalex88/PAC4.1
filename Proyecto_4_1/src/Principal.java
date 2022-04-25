3c2b83d9c3ea422d342f0fa6cc4676bd9380c0d4
Manuel Alejandro Márquez <manuelalejandromarquezlopez@iestorredelrey.es>
2022-04-25 09:43:52 +0200

    Final

--- Proyecto_4_1/src/Principal.java
+++ Proyecto_4_1/src/Principal.java
@@ -10,7 +10,6 @@ public class Principal {
 		System.out.println(jmbola.getApellido());	
 		System.out.println(jmbola.getNombre());
 		
-		
 		MManuelAlejandroMarquezLopez1 maml = new MManuelAlejandroMarquezLopez1("Manuel Alejandro", "Marquez Lopez");
 		System.out.println(maml.getApellido());	
 		System.out.println(maml.getNombre());
