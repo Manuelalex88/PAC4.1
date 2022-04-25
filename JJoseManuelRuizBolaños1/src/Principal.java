3677b0077678ca1f8044e3b406d1d47fdd3aefff
Usuario <Usuario@DESKTOP-OA178EB>
2022-04-25 08:48:37 +0200

    Final

--- JJoseManuelRuizBolaños1/src/Principal.java
+++ JJoseManuelRuizBolaños1/src/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Buenas tardes grupo");
+		
 		JJoseManuelRuizBolaños1 jmbola = new JJoseManuelRuizBolaños1("Jose Manuel", "Ruiz Bolaños");
 		System.out.println(jmbola.getApellido());	
 		System.out.println(jmbola.getNombre());
